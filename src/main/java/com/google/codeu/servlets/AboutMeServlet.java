6156a1e42abdc74c4d3ee0a95ce07296893572c5
mabdullahrafique <mrafique.bese17seecs@seecs.edu.pk>
2019-06-02 16:51:12 +0500

    Updated appengine file to resolve conflicts

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,25 +1,13 @@
 package com.google.codeu.servlets;
-
-
-import java.io.IOException;
-
-
-import javax.servlet.annotation.WebServlet;
-
-import javax.servlet.http.HttpServlet;
-
-import javax.servlet.http.HttpServletRequest;
-
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.appengine.api.users.UserService;
-
 import com.google.appengine.api.users.UserServiceFactory;
-
 import com.google.codeu.data.Datastore;
-
 import com.google.codeu.data.User;
-
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
@@ -32,81 +20,50 @@ import org.jsoup.safety.Whitelist;
 
 @WebServlet("/about")
 
-public class AboutMeServlet extends HttpServlet{
-
+public class AboutMeServlet extends HttpServlet {
   private Datastore datastore;
-
-  public void init(){
-
+  public void init() {
     datastore = new Datastore();
-
   }
 
  @Override
-
  public void doGet(HttpServletRequest request, HttpServletResponse response)
  throws IOException
  {
-
    try
-{
+ {
     // statement(s) that might cause exception
-
-
-  response.setContentType("text/html");
-  String user_email = request.getParameter("user");
-  if (user_email == null || user_email == ""){
+  String userEmail = request.getParameter("user");
+  if (userEmail == null || userEmail == ""){
     // So request is invalid
     return;
   }
 
-  User user = datastore.getUser(user_email);
-
-  String user_about_me = user.getAboutMe();
-
-
-
-
-  response.getOutputStream().println(user_about_me);
-
+  User user = datastore.getUser(userEmail);
+  String userAboutMe = user.getAboutMe();
+  response.getOutputStream().println(userAboutMe);
  }
-
  catch(Exception e)
  {
-
    response.getOutputStream().println("Please add AboutMe");
-
  }
-
-
 }
 
  public void doPost(HttpServletRequest request, HttpServletResponse response)
   throws IOException {
 
     UserService userService = UserServiceFactory.getUserService();
-
     if (!userService.isUserLoggedIn()){
       response.sendRedirect("/index.html");
       return;
     }
 
     String userEmail = userService.getCurrentUser().getEmail();
-
     String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
     // String aboutMe = request.getParameter("about-me");
-
     User user = new User(userEmail, aboutMe);
     datastore.storeUser(user);
-
-
-
     System.out.println("Saving me for " + userEmail);
-
     response.sendRedirect("/user-page.html?user="+userEmail);
-
-
-
   }
-
 }
